e2e66f20f3fc03a2f9e1266b0166afafde28d256
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2025-06-17 17:42:11 +0200

    shorten summary prompt

--- src/packagerix/packaging_flow/model_prompts.py
+++ src/packagerix/packaging_flow/model_prompts.py
@@ -52,12 +52,6 @@ Note: Even though the provided template uses the mkDerivation function, this is 
 @ask_model("""@model You are software packaging expert who can build any project using the Nix programming language.
 
 Read the contents of the project's GitHub page and summarize it.
-Include information like
-    1. The build tool which should be invoked to build the project.
-    2. A list of build tools and project dependencies.
-    3. Other information which might be necessary for buiding the project.
-    
-    Do not include information which is irrelevant for building the project.
 Here is the information form the project's GitHub page:
 
 ```text
