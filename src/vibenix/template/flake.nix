2dc4a3320abfd0ea61d76697bb96b0cc31b73a3a
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-07-05 16:12:11 +0200

    improve log quality by setting environment variables during build

--- src/vibenix/template/flake.nix
+++ src/vibenix/template/flake.nix
@@ -8,10 +8,33 @@
   outputs = { self, nixpkgs }:
   let
     pkgs = import nixpkgs { system = "x86_64-linux"; };
+    ciStdenv = pkgs.stdenv // {
+      mkDerivation = args: pkgs.stdenv.mkDerivation (args // {
+        preBuild = ''
+          ${args.preBuild or ""}
+          # CI/Non-interactive environment
+          export CI=true
+          export DEBIAN_FRONTEND=noninteractive
+          export NONINTERACTIVE=1
+          
+          # Terminal and color control
+          export TERM=dumb
+          export NO_COLOR=1
+          export FORCE_COLOR=0
+          export CLICOLOR=0
+          export CLICOLOR_FORCE=0
+          
+          # Disable progress bars and spinners
+          export PROGRESS_NO_TRUNC=1
+        '';
+      });
+    };
   in
    {
 
-    packages.x86_64-linux.default = pkgs.callPackage ./package.nix {};
+    packages.x86_64-linux.default = pkgs.callPackage ./package.nix {
+      stdenv = ciStdenv;
+    };
     packages.x86_64-linux.nixpkgs-src = nixpkgs.outPath;
 
   };
