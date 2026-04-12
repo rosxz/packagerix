7c845c590661d3b836b7fa92d3ffc0c17062c69b
Martim Moniz <martim@moniz.pt>
2026-04-12 15:44:43 +0100

    fix: wrong zulip maintenance dataset, add alternative update strategies nix-update

--- research/maintenance_feb/zulip.nix
+++ research/maintenance_feb/zulip.nix
@@ -1,87 +1,55 @@
 {
   lib,
+  buildPythonPackage,
+  pythonOlder,
   fetchFromGitHub,
-  buildNpmPackage,
-  python3,
-  electron_35,
-  makeDesktopItem,
-  makeShellWrapper,
-  copyDesktopItems,
+  setuptools,
+  requests,
+  distro,
+  click,
+  typing-extensions,
+  matrix-nio,
+  pytestCheckHook,
 }:
 
-buildNpmPackage rec {
+buildPythonPackage rec {
   pname = "zulip";
-  version = "5.12.0";
+  version = "0.9.0";
 
+  disabled = pythonOlder "3.8";
+
+  pyproject = true;
+
+  # no sdist on PyPI
   src = fetchFromGitHub {
     owner = "zulip";
-    repo = "zulip-desktop";
-    tag = "v${version}";
-    hash = "sha256-YDb69tJCR58DARssnZgdVxtRpR8vHsawCTv7kQ56y+8=";
+    repo = "python-zulip-api";
+    rev = version;
+    hash = "sha256-YnNXduZ2KOjRHGwhojkqpMP2mwhflk8/f4FVZL8NvHU=";
   };
+  sourceRoot = "${src.name}/zulip";
 
-  npmDepsHash = "sha256-MKKN6prUdWaHm27GybdbswDMNJH0xVffXsT2ZwroOHI=";
+  nativeBuildInputs = [ setuptools ];
 
-  env = {
-    ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
-  };
+  propagatedBuildInputs = [
+    requests
+    distro
+    click
+    typing-extensions
+  ]
+  ++ requests.optional-dependencies.security;
 
-  nativeBuildInputs = [
-    makeShellWrapper
-    copyDesktopItems
-    (python3.withPackages (ps: with ps; [ distutils ]))
+  nativeCheckInputs = [
+    matrix-nio
+    pytestCheckHook
   ];
 
-  dontNpmBuild = true;
-  buildPhase = ''
-    runHook preBuild
-
-    npm run pack -- \
-      -c.electronDist=${electron_35}/libexec/electron \
-      -c.electronVersion=${electron_35.version}
-
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p "$out/share/lib/zulip"
-    cp -r dist/*-unpacked/resources/app.asar* "$out/share/lib/zulip/"
-
-    install -m 444 -D app/resources/zulip.png $out/share/icons/hicolor/512x512/apps/zulip.png
-
-    makeShellWrapper '${lib.getExe electron_35}' "$out/bin/zulip" \
-      --add-flags "$out/share/lib/zulip/app.asar" \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-wayland-ime=true}}" \
-      --inherit-argv0
-
-    runHook postInstall
-  '';
-
-  desktopItems = [
-    (makeDesktopItem {
-      name = "zulip";
-      exec = "zulip %U";
-      icon = "zulip";
-      desktopName = "Zulip";
-      comment = "Zulip Desktop Client for Linux";
-      categories = [
-        "Chat"
-        "Network"
-        "InstantMessaging"
-      ];
-      startupWMClass = "Zulip";
-      terminal = false;
-    })
-  ];
+  pythonImportsCheck = [ "zulip" ];
 
   meta = with lib; {
-    description = "Desktop client for Zulip Chat";
-    homepage = "https://zulip.com";
+    description = "Bindings for the Zulip message API";
+    homepage = "https://github.com/zulip/python-zulip-api";
     license = licenses.asl20;
-    maintainers = with maintainers; [ andersk ];
-    platforms = lib.platforms.linux;
-    mainProgram = "zulip";
+    maintainers = with maintainers; [ dotlambda ];
   };
 }
